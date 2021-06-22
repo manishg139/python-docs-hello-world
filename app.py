dc5430f7e71630b759f6e6aa3d1da87fe5da9ee2
manishg139 <49610409+manishg139@users.noreply.github.com>
2021-06-22 15:09:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    #return "Hello, World!"
+    return render_template("login.html")
